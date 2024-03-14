47661c90f7cbf3d8b3153d632c8476b3613f9a86
Xeno852 <lblackwe@andrew.cmu.edu>
2024-03-14 16:01:27 -0400

    name changed 2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
-  if (query.includes("What is your name?")) {
+  else if (query.includes("name?")) {
     return (
       "Luke Blackwell"
     );
